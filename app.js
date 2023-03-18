f19220afc1712a5b3e75b8c9e05430c3c058200a
jainpricita <pricitajain@gmail.com>
2023-03-18 17:11:59 +0530

    added animate for infinite carousel

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const carousel= document.querySelector(".carousel");
+const carousel = document.querySelector(".carousel");
 
-let sliders=[];
+let sliders = [];
 
-let currentSlide=0;
+let currentSlide = 0;
 
-function createSlide(){
-    if(currentSlide>=movies.length){
-        currentSlide=0;
+function createSlide() {
+    if (currentSlide >= movies.length) {
+        currentSlide = 0;
     }
-   
 
-// console.log(currentSlide);
+
+    // console.log(currentSlide);
 
 
     //creating DOM elements 
-    let slide=document.createElement("div");
-    let slideDetails=document.createElement("div");
-    let title=document.createElement("h1");
-    let data=document.createElement("div");
-    let movieDetails=document.createElement("p");
-    let type=document.createElement("span");
-    let year=document.createElement("span");
-    let period=document.createElement("i");
-    let image=document.createElement("img");
+    let slide = document.createElement("div");
+    let slideDetails = document.createElement("div");
+    let title = document.createElement("h1");
+    let data = document.createElement("div");
+    let movieDetails = document.createElement("p");
+    let type = document.createElement("span");
+    let year = document.createElement("span");
+    let period = document.createElement("i");
+    let image = document.createElement("img");
 
 
     //attaching all elements
@@ -40,37 +40,132 @@ function createSlide(){
     slide.appendChild(image);
     carousel.appendChild(slide);
 
-    image.src=movies[currentSlide].image;
+    image.src = movies[currentSlide].image;
     currentSlide++;
 
     //classnames
-    slide.className="slider";
-    slideDetails.className="slide-details";
-    title.className="movie-title";
-    data.className="movie-data";
-    period.className="fa-solid fa-circle data-first";
-    movieDetails.className="movie-details";
+    slide.className = "slider";
+    slideDetails.className = "slide-details";
+    title.className = "movie-title";
+    data.className = "movie-data";
+    period.className = "fa-solid fa-circle data-first";
+    movieDetails.className = "movie-details";
 
 
-    
-     sliders.push(slide);
-    
 
-    //adding a slide effect
-    let shift=(-100*(sliders.length-2));
-    let shift2=(-14*(sliders.length-2));
-    // console.log(shift,shift2);
-    if(sliders.length){
-         sliders[0].style.marginLeft= "calc("+shift+"% + "+shift2+"px)";
-// console.log(sliders[0].style.marginLeft);
-    };
-    
+    sliders.push(slide);
+
+         //adding a slide effect by adding margin left to the first slide, was increasing the length of array Everytime it was called
+
+        // let shift=(-100*(sliders.length-2));
+        // let shift2=(-14*(sliders.length-2));
+        // console.log(shift,shift2);
+        // if(sliders.length){
+        //      sliders[0].style.marginLeft= "calc("+shift+"% + "+shift2+"px)";
+    // console.log(sliders[0].style.marginLeft);
+        // };
 }
 
-for(let i=0;i<3;i++){
+//i<=length, one duplicate item needed to make smooth transition
+for (let i = 0; i <=movies.length ; i++) {
     createSlide();
-}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------Trying out different logics to start the array again and not increase sliders length-------------
+
+// let index=1;
+// setInterval(function(){
+//     if(index>=sliders.length){
+//         index=1;
+//     }
+//     let move=-101.8*index;
+//     sliders[0].style.marginLeft= move+"%";
+//     index++;
+// },1000);
+
+
+// sliders[1].style.marginLeft="-187.5%";
 
 // setInterval(function(){
+//  createSlide();
+// },3000);
+
+
+
+// if (currentSlide <= movies.length){
+//     for (let i = 0; i < movies.length; i++) {
+//         createSlide();
+//     }
+
+//     setInterval(function () {
+//         i=1;
+//         if(i>movies.length){
+//             i=1;
+//         }
+//         moveSlide(i);
+//         i++;
+//     }, 2000);
+// }
+
+
+
+
+// for (let i = 0; i < movies.length; i++) {
 //     createSlide();
-// },2000);
+// }
+// for(let i=1;i<sliders.length;i++){
+//     moveSlide(i);
+// }
+
+// function moveSlide(i) {
+//     //adding a slide effect
+//     let shift = (-100 * (i - 2));
+//     let shift2 = (-14 * (i - 2));
+//     // console.log(shift,shift2);
+//     if (sliders.length) {
+//         sliders[0].style.marginLeft = "calc(" + shift + "% + " + shift2 + "px)";
+//         // console.log(sliders[0].style.marginLeft);
+//     };
+// }
+
+
+
+
+// let index=0;
+//  setInterval(function () {
+//   moveSlide(index);
+//  }, 2000);
+
+// const moveSlide = (index)=>{
+//     this.setInterval(() => {
+//         let shift = (-100 * (sliders[index] - 2));
+//     let shift2 = (-14 * (sliders[index] - 2));
+//     // console.log(shift,shift2);
+//     if (sliders.length) {
+//         sliders[0].style.marginLeft = "calc(" + shift + "% + " + shift2 + "px)";}
+  
+//         index++;
+//       }, 2000);
+//     };
+
+//     moveSlide(3);
+
+
